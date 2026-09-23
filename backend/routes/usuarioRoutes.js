f8aec3e37f4ab0f0f67c26a8c711ab6b19a89768
Giovanna <giovannaferreiracampos34@gmail.com>
2026-09-23 10:18:55 -0300

    Organização das pastas - Atualização dos caminhos html, css, js

--- backend/routes/usuarioRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const UsuarioController = require("../controller/usuarioController");
-
-router.post("/cadastro", UsuarioController.cadastrar);
-router.post("/login", UsuarioController.login);
-
-module.exports = router;
